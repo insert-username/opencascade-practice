4225991b289dfd5e262d70bd7d28a6798c83c8df
insert-username <insert-username@users.noreply.github.com>
2021-01-04 19:29:01 +0100

    Some UI design stuff.

--- qt-example/mainwindow.cpp
+++ qt-example/mainwindow.cpp
@@ -1,13 +1,19 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include <QGridLayout>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    this->occView = new OccView(this);
-    this->setCentralWidget(this->occView);
+    auto* viewportWidget = ui->centralwidget->findChild<QWidget*>("viewportWidget");
+
+    this->occView = new OccView(viewportWidget);
+
+    auto* layout = new QGridLayout(viewportWidget);
+    layout->addWidget(this->occView);
 
     connect(ui->actionAddBox, SIGNAL(triggered()), this, SLOT(addBox()));
 }
@@ -17,7 +23,6 @@ void MainWindow::addBox() {
     TopoDS_Shape shape = BRepPrimAPI_MakeBox(3.0, 4.0, 5.0).Shape();
     Handle(AIS_Shape) aisShape = new AIS_Shape(shape);
     aisShape->SetColor(Quantity_NOC_AZURE);
-
 
     std::cout << "Adding box to context" << std::endl;
     this->occView->context->Display(aisShape, Standard_True);
