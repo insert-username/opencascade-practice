5740f8d3d464c75073796d7ef3df0f6f0d789fa7
insert-username <insert-username@users.noreply.github.com>
2021-01-08 22:00:20 +0100

    Make MainWindow header only for easier development.

--- qt-example/mainwindow.h
+++ qt-example/mainwindow.h
@@ -1,10 +1,13 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
+#include "./ui_mainwindow.h"
 #include "occview.h"
+
+#include <QMainWindow>
 #include <AIS_Shape.hxx>
 #include <BRepPrimAPI_MakeBox.hxx>
+#include <QGridLayout>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -13,16 +16,56 @@ QT_END_NAMESPACE
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
-public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
-
-protected slots:
-    void addBox();
-
 private:
     Ui::MainWindow *ui;
     OccView* occView;
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr)
+            : QMainWindow(parent)
+            , ui(new Ui::MainWindow)
+    {
+        ui->setupUi(this);
+        auto* viewportWidget = ui->centralwidget->findChild<QWidget*>("viewportWidget");
+
+        this->occView = new OccView(viewportWidget);
+
+        auto* layout = new QGridLayout(viewportWidget);
+        layout->addWidget(this->occView);
+
+        connect(ui->actionAddBox, SIGNAL(triggered()), this, SLOT(addBox()));
+    }
+
+    ~MainWindow() override {
+        delete ui;
+    }
+
+protected slots:
+    void addBox() {
+        std::cout << "Adding box" << std::endl;
+        double xSize = 3.0 * static_cast<float>(std::rand()) / static_cast<float>(RAND_MAX);
+        double ySize = 3.0 * static_cast<float>(std::rand()) / static_cast<float>(RAND_MAX);
+        double zSize = 3.0 * static_cast<float>(std::rand()) / static_cast<float>(RAND_MAX);
+
+        TopoDS_Shape shape = BRepPrimAPI_MakeBox(xSize, ySize, zSize).Shape();
+        Handle(AIS_Shape) aisShape = new AIS_Shape(shape);
+
+        Graphic3d_MaterialAspect matAspect;
+        matAspect.SetDiffuseColor(Quantity_NOC_BEET);
+        matAspect.SetAmbientColor(Quantity_NOC_BEET);
+        matAspect.SetSpecularColor(Quantity_NOC_GREEN1);
+
+        aisShape->SetMaterial(matAspect);
+
+        this->occView->context->Display(aisShape, Standard_True);
+
+        this->occView->context->Load(aisShape, -1);
+        this->occView->context->Activate(aisShape, TopAbs_FACE);
+
+        this->occView->view->FitAll();
+        this->occView->view->ZFitAll();
+        this->occView->view->Redraw();
+    };
+
 };
 #endif // MAINWINDOW_H
